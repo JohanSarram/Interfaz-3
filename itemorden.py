bc84c31521e78ea602c623b9518e881d8f404f97
JohanSarram <ander.sarram@gmail.com>
2024-06-21 23:31:18 -0500

    Delete itemorden.py

--- itemorden.py
+++
@@ -1,9 +0,0 @@
-# itemorden.py
-
-class ItemOrden:
-    def __init__(self, producto, cantidad):
-        self.producto = producto
-        self.cantidad = cantidad
-    
-    def calcular_subtotal(self):
-        return self.producto.precio * self.cantidad
